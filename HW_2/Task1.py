598d58413bd7efd9946b733b649d0a2cf78ee852
AHilarov <ahilarov@yandex.ru>
2023-02-19 02:01:35 +0300

    Домашка 2. Задача 1

---
+++ HW_2/Task1.py
@@ -0,0 +1,15 @@
+# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом.
+# Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были
+# повернуты вверх одной и той же стороной. Выведите минимальное
+# количество монет, которые нужно перевернуть.
+# 5 -> 1 0 1 1 0
+# 2
+
+import random
+n = int(input('Enter number of coins: '))
+a = [random.randint(0, 1) for i in range(n)]
+print(a)
+if a.count(0) < a.count(1) or a.count(0) == a.count(1):
+    print('You need to turn', a.count(0), 'coins')
+else:
+    print('You need to turn', a.count(1), 'coins')
